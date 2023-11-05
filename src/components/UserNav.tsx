8cebe0142391f1a5e53127de1edd89069d1c1293
Shazid <shazid.dev@gmail.com>
2023-11-05 19:54:53 +0600

    Providers wrapper added

--- src/components/UserNav.tsx
+++ src/components/UserNav.tsx
@@ -3,8 +3,6 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import Image from "next/image";
