51c78e27eb0858cfdc45348d826fc4f842cd7b31
Shazid <shazid.dev@gmail.com>
2023-11-07 19:01:17 +0600

    quiz form changes and database model updated

--- src/components/CreateQuiz.tsx
+++ src/components/CreateQuiz.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import {
   Card,
@@ -6,20 +8,115 @@ import {
   CardHeader,
   CardTitle,
 } from "./ui/card";
+import { useForm } from "react-hook-form";
+import { createQuizSchema } from "@/schemas/form/quiz";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "./ui/button";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+} from "./ui/form";
+import { BookOpen, CopyCheck } from "lucide-react";
 
 type Props = {};
+type Input = z.infer<typeof createQuizSchema>;
 
 const CreateQuiz = (props: Props) => {
+  const form = useForm<Input>({
+    resolver: zodResolver(createQuizSchema),
+    defaultValues: {
+      amount: 3,
+      topic: "",
+      type: "open_ended",
+    },
+  });
+  form.watch();
+
+  const onSubmit = (input: Input) => {
+    alert(JSON.stringify(input, null, 2));
+  };
   return (
     <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
       <Card>
         <CardHeader>
           <CardTitle className="text-2xl">Create a Quiz</CardTitle>
-          <CardDescription>
-            <p>Choose a topic</p>
-          </CardDescription>
+          <CardDescription>Choose a topic</CardDescription>
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="topic"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Topic</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter a topic..." {...field} />
+                    </FormControl>
+                    <FormDescription>Please provide a topic</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Number of Questions</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter an amount"
+                        {...field}
+                        onChange={(e) => {
+                          form.setValue("amount", parseInt(e.target.value));
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="grid grid-cols-2 gap-2">
+                <Button
+                  type="button"
+                  variant={
+                    form.getValues("type") === "mcq" ? "default" : "secondary"
+                  }
+                  onClick={() => {
+                    form.setValue("type", "mcq");
+                  }}
+                >
+                  <CopyCheck className="mr-2 h-5 w-5" />
+                  Multiple Choice
+                </Button>
+                <Button
+                  type="button"
+                  variant={
+                    form.getValues("type") === "open_ended"
+                      ? "default"
+                      : "secondary"
+                  }
+                  onClick={() => {
+                    form.setValue("type", "open_ended");
+                  }}
+                >
+                  <BookOpen className="mr-2 h-5 w-5" />
+                  Open Ended
+                </Button>
+              </div>
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
       </Card>
     </div>
   );
---
+++ src/schemas/form/quiz.ts
@@ -0,0 +1,7 @@
+import {z} from 'zod'
+
+export const createQuizSchema = z.object({
+    topic: z.string().min(4,{message:"Topic must beat least 4 characters long"}),
+    type: z.enum(['mcq','open_ended']),
+    amount: z.number().min(1).max(10)
+})
