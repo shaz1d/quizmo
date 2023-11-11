5151af324755f549424f0e6e00443732cf9f8de7
Shazid <shazid.dev@gmail.com>
2023-11-12 01:46:22 +0600

    commit from laptop

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -64,7 +64,7 @@ export async function POST(req: Request, res: Response) {
         }
       );
     } else {
-      console.error("elle gpt error", error);
+      console.error("shazid gpt error", error);
       return NextResponse.json(
         { error: "An unexpected error occurred." },
         {
