146724832af8bfc224ee459ce8b3f62f23eca917
Shazid <shazid.dev@gmail.com>
2023-11-14 17:54:31 +0600

    check answer api route created

--- src/schemas/form/quiz.ts
+++ src/schemas/form/quiz.ts
@@ -1,7 +1,14 @@
-import {z} from 'zod'
+import { z } from "zod";
 
 export const createQuizSchema = z.object({
-    topic: z.string().min(4,{message:"Topic must beat least 4 characters long"}),
-    type: z.enum(['mcq','open_ended']),
-    amount: z.number().min(1).max(10)
-})
+  topic: z
+    .string()
+    .min(4, { message: "Topic must beat least 4 characters long" }),
+  type: z.enum(["mcq", "open_ended"]),
+  amount: z.number().min(1).max(10),
+});
+
+export const checkAnswerSchema = z.object({
+  questionId: z.string(),
+  userAnswer: z.string(),
+});
