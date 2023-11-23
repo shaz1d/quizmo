9a345f92fcc0f1a7d1473ac0196a308be5e742f3
Shazid <shazid.dev@gmail.com>
2023-11-23 12:50:42 +0600

    question generate issue

--- src/app/api/game/route.ts
+++ src/app/api/game/route.ts
@@ -3,10 +3,10 @@ import { NextResponse } from "next/server";
 import { createQuizSchema } from "@/schemas/form/quiz";
 import { ZodError } from "zod";
 import { prisma } from "@/lib/db";
-import axios from "axios";
+import { strict_output } from "@/lib/gpt";
 
 export const runtime = "nodejs";
-export const maxDuration = 10;
+export const maxDuration = 100;
 
 export async function POST(req: Request, res: Response) {
   try {
@@ -23,14 +23,33 @@ export async function POST(req: Request, res: Response) {
     }
     const body = await req.json();
     const { amount, topic, type } = createQuizSchema.parse(body);
-    const { data } = await axios.post(
-      `${process.env.API_URL as string}/api/questions`,
-      {
-        amount,
-        topic,
-        type,
-      }
-    );
+    let questions: any;
+    if (type === "open_ended") {
+      questions = await strict_output(
+        "You are a helpful AI that is able to generate a pair of question and answers, the length of each answer should not be more than 15 words, store all the pairs of answers and questions in a JSON array",
+        new Array(amount).fill(
+          `You are to generate a random hard open-ended questions about ${topic}`
+        ),
+        {
+          question: "question",
+          answer: "answer with max length of 15 words",
+        }
+      );
+    } else if (type === "mcq") {
+      questions = await strict_output(
+        "You are a helpful AI that is able to generate mcq questions and answers, the length of each answer should not be more than 15 words, store all answers and questions and options in a JSON array",
+        new Array(amount).fill(
+          `You are to generate a random hard mcq question about ${topic}`
+        ),
+        {
+          question: "question",
+          answer: "answer with max length of 15 words",
+          option1: "option1 with max length of 15 words",
+          option2: "option2 with max length of 15 words",
+          option3: "option3 with max length of 15 words",
+        }
+      );
+    }
     const game = await prisma.game.create({
       data: {
         userId: session.user.id,
@@ -48,7 +67,7 @@ export async function POST(req: Request, res: Response) {
         option2: string;
         option3: string;
       };
-      let manyData = data.questions.map((question: mcqQuestion) => {
+      let manyData = questions.map((question: mcqQuestion) => {
         let options = [
           question.answer,
           question.option1,
@@ -72,7 +91,7 @@ export async function POST(req: Request, res: Response) {
         question: string;
         answer: string;
       };
-      let manyData = data.questions.map((question: openEndedQuestion) => {
+      let manyData = questions.map((question: openEndedQuestion) => {
         return {
           question: question.question,
           answer: question.answer,
